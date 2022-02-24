432cdfde4feffba467447f19b5a5a2e770b9c737
Ives Moreira Gomes <ivesmoreira3@gmail.com>
2022-02-24 12:34:19 -0300

    feat: games buttons dinamic

--- src/index.js
+++ src/index.js
@@ -1,84 +1,117 @@
-(function(win, doc) {
+(function(_win, doc) {
   'use strict';
 
-  var app = (function() {
-    var data;
-    var $game = [];
-    var $gameName = '';
-    var $totalPrice = 0;
+  const app = (function() {
+    let apiGamesData;
+    let currentGame = [];
+    let currentGameName = '';
+    let currentTotalPrice = 0;
 
     return {
       init: function init() {
         app.getAppData();
-        app.listenGameButtons();
         app.gameActions();
         app.startCart();
       },
 
       getAppData: function getAppData() {
-        var ajax = new XMLHttpRequest();
+        let ajax = new XMLHttpRequest();
 
         ajax.open('GET', '../utils/data.json', true);
         ajax.send();
         ajax.addEventListener('readystatechange', app.showAppData, false);
       },
 
-      listenGameButtons: function listenGameButtons() {
-        var $gameButtons = doc.querySelectorAll('[data-js="game-button"]');
+      createGameButtons: function createGameButtons() {
+        let sectionButtons = doc.querySelector('[data-js="section-buttons"]');
 
-        Array.prototype.map.call($gameButtons, function(button) {
+        Array.prototype.map.call(apiGamesData.types, function(game) {
+          let gameButton = doc.createElement('button');
+          gameButton.setAttribute('data-js', 'game-button');
+
+          gameButton.textContent = game.type;
+
+          gameButton.style.backgroundColor = '#fff';
+          gameButton.style.color = game.color;
+          gameButton.style.border = `2px solid ${game.color}`
+
+          sectionButtons.appendChild(gameButton);
+        });
+
+        app.listenGameButtons();
+
+        if (sectionButtons.childNodes[0]) {
+          app.selectButton(sectionButtons.childNodes[0]);
+        }
+      },
+
+      listenGameButtons: function listenGameButtons() {
+        let gameButtons = doc.querySelectorAll('[data-js="game-button"]');
+
+        Array.prototype.map.call(gameButtons, function(button) {
           button.addEventListener('click', function() {
-            app.selectButton(button, $gameButtons);
+            app.selectButton(button);
           }, false);
         });
       },
 
       selectButton: function selectButton(button) {
-        var $gameButtons = doc.querySelector('[data-js="game-buttons"]');
+        let gameButtons = doc.querySelector('[data-js="game-buttons"]');
 
-        $game = [];
+        let othersButtons = [];
 
-        data.types.map(function(game) {
+        currentGame = [];
+
+        apiGamesData.types.map(function(game) {
           if (button.textContent.trim() === game.type) {
+            app.focusGameButton(button, game.color);
             app.showGameInfos(game);
 
-            $gameButtons.innerHTML = '';
+            gameButtons.innerHTML = '';
 
-            for (var i = 0; i < game.range; i++) {
-              var $buttonGameNumber = doc.createElement('button');
+            for (let i = 0; i < game.range; i++) {
+              let buttonGameNumber = doc.createElement('button');
 
-              $buttonGameNumber.setAttribute('data-js', 'button-game-number');
-              $buttonGameNumber.textContent = (i + 1);
+              buttonGameNumber.setAttribute('data-js', 'button-game-number');
+              buttonGameNumber.textContent = (i + 1);
 
-              app.gameButtonListener($buttonGameNumber, game);
+              app.gameButtonListener(buttonGameNumber, game);
 
-              $gameButtons.appendChild($buttonGameNumber);
+              gameButtons.appendChild(buttonGameNumber);
             }
 
-            $gameName = game.type;
+            currentGameName = game.type;
+          } else {
+            othersButtons.push({ 
+              gameType: game.type,
+              gameColor: game.color
+            });
           }
         });
 
-        app.focusGameButton(button);
+        app.defocusGameButton(othersButtons);
       },
 
-      focusGameButton: function focusGameButton(button) {
-        var $gameButtons = doc.querySelectorAll('[data-js="game-button"]');
-        var $classButton = button.getAttribute('class');
-
-        button.setAttribute('class', $classButton.concat('-focus'));
-
-        Array.prototype.some.call($gameButtons, function(gameButton) {
-          var $classGameButton = gameButton.getAttribute('class');
-          var $index = $classGameButton.indexOf('-');
-
-          if (gameButton !== button && $index !== -1) {
-            var $className = $classGameButton.slice(0, $index);
-
-            gameButton.setAttribute('class', $className);  
-          }
-        });
+      focusGameButton: function focusGameButton(button, game) {
+        button.style.backgroundColor = game;
+        button.style.color = '#fff';
+        button.style.border = `2px solid ${game}`
       },  
+
+      defocusGameButton: function focusGameButton(othersButtons) {
+        let gameButtons = doc.querySelectorAll('[data-js="game-button"]');
+
+        Array.prototype.forEach.call(gameButtons, function(gameButton) {
+          othersButtons.map(function(game) {
+            if (gameButton.textContent === game.gameType) {
+              gameButton.style.backgroundColor = '#fff';
+              gameButton.style.color = game.gameColor;
+              gameButton.style.border = `2px solid ${game.gameColor}`
+            }
+          });
+        });
+
+      },
 
       gameButtonListener: function gameButtonListener(button, game) {
         button.addEventListener('click', function() {
@@ -87,89 +120,89 @@
       },
 
       game: function game(button, gameNumber, game) {
-        if ($game.length >= game.max_number) {
+        if (currentGame.length >= game.max_number) {
           return;
         }
 
-        var $alreadyExist = $game.some(function(n) {
+        let alreadyExist = currentGame.some(function(n) {
           return n === gameNumber;
         });
 
-        if ($alreadyExist) {
+        if (alreadyExist) {
           return;
         }
 
         button.setAttribute('class', 'selected-game');
 
-        $game.push(gameNumber);
+        currentGame.push(gameNumber);
       },
 
       gameActions: function gameActions() {
-        var $completeGame = doc.querySelector('[data-js="complete-game"]');
-        var $clearGame = doc.querySelector('[data-js="clear-game"]');
-        var $addToCart = doc.querySelector('[data-js="add-to-cart"]');
+        let completeGame = doc.querySelector('[data-js="complete-game"]');
+        let clearGame = doc.querySelector('[data-js="clear-game"]');
+        let addToCart = doc.querySelector('[data-js="add-to-cart"]');
 
-        $completeGame.addEventListener('click', function() {
+        completeGame.addEventListener('click', function() {
           app.completeGame();
         }, false);
 
-        $clearGame.addEventListener('click', function() {
+        clearGame.addEventListener('click', function() {
           app.clearGame();
         }, false);
 
-        $addToCart.addEventListener('click', function() {
+        addToCart.addEventListener('click', function() {
           app.createGame();
         }, false);
       },
 
       createGame: function createGame() {
-        Array.prototype.map.call(data.types, function(game) {
-          if (game.type === $gameName) {
-            if (game.max_number > $game.length) {
+        Array.prototype.map.call(apiGamesData.types, function(game) {
+          if (game.type === currentGameName) {
+            if (game.max_number > game.length) {
               return;
             }
 
-            var $asideCartItem = doc.createElement('div');
-            $asideCartItem.setAttribute('class', 'aside-cart-item');
+            let asideCartItem = doc.createElement('div');
+            asideCartItem.setAttribute('class', 'aside-cart-item');
 
-            var $button = doc.createElement('button');
+            let button = doc.createElement('button');
 
-            var $icon = doc.createElement('span');
-            $icon.setAttribute('class', 'material-icons');
-            $icon.textContent = 'delete';
+            let icon = doc.createElement('span');
+            icon.setAttribute('class', 'material-icons');
+            icon.textContent = 'delete';
 
-            $button.appendChild($icon);
-            app.listenDelete($button);
+            button.appendChild(icon);
+            app.listenDelete(button);
 
-            var $divisor = doc.createElement('div'); 
-            $divisor.setAttribute('class', 'aside-cart-item-divisor');
-            $divisor.setAttribute('data-js', 'divisor');
+            let divisor = doc.createElement('div'); 
+            divisor.setAttribute('class', 'aside-cart-item-divisor');
+            divisor.setAttribute('data-js', 'divisor');
 
-            var $asideCartItemNumbers = doc.createElement('div');
-            $asideCartItemNumbers.setAttribute('class', 'aside-cart-item-numbers');
+            let asideCartItemNumbers = doc.createElement('div');
+            asideCartItemNumbers.setAttribute('class', 'aside-cart-item-numbers');
 
-            var $gameNumbers = doc.createElement('p');
-            $gameNumbers.setAttribute('data-js', 'game-numbers');
+            let gameNumbers = doc.createElement('p');
+            gameNumbers.setAttribute('data-js', 'game-numbers');
 
-            $asideCartItemNumbers.appendChild($gameNumbers);
+            asideCartItemNumbers.appendChild(gameNumbers);
 
-            var $asideCartItemGame = doc.createElement('div');
-            $asideCartItemGame.setAttribute('class', 'aside-cart-item-game');
-            var $name = doc.createElement('p');
-            $name.setAttribute('data-js', 'game-name');
-            var $gamePrice = doc.createElement('span');
-            $gamePrice.setAttribute('data-js', 'game-price');
+            let asideCartItemGame = doc.createElement('div');
+            asideCartItemGame.setAttribute('class', 'aside-cart-item-game');
+            let name = doc.createElement('p');
+            name.setAttribute('data-js', 'game-name');
+            let gamePrice = doc.createElement('span');
+            gamePrice.setAttribute('data-js', 'game-price');
 
-            $asideCartItemGame.appendChild($name);
-            $asideCartItemGame.appendChild($gamePrice);
+            asideCartItemGame.appendChild(name);
+            asideCartItemGame.appendChild(gamePrice);
 
-            $asideCartItemNumbers.appendChild($asideCartItemGame);
+            asideCartItemNumbers.appendChild(asideCartItemGame);
 
-            $asideCartItem.appendChild($button);
-            $asideCartItem.appendChild($divisor);
-            $asideCartItem.appendChild($asideCartItemNumbers);
+            asideCartItem.appendChild(button);
+            asideCartItem.appendChild(divisor);
+            asideCartItem.appendChild(asideCartItemNumbers);
 
-            app.showInCart($asideCartItem);
+            app.showInCart(asideCartItem);
             
             return;
           }
@@ -179,8 +212,8 @@
       completeGame: function completeGame() {
         app.clearGame();
 
-        Array.prototype.map.call(data.types, function(game) {
-          if (game.type === $gameName) {
+        Array.prototype.map.call(apiGamesData.types, function(game) {
+          if (game.type === currentGameName) {
             app.randomGame(game.max_number, game.range);
 
             return;
@@ -189,80 +222,96 @@
       },
 
       randomGame: function randomGame(max_number, range) {
-        for (var i = 0; i < max_number; i++) {
-          var $random = Math.floor(Math.random() * (range - 1)) + 1;
+        for (let i = 0; i < max_number; i++) {
+          let random = Math.floor(Math.random() * (range - 1)) + 1;
 
-          var $alreadyExist = $game.some(function(number) {
-            return number === $random;
+          let alreadyExist = currentGame.some(function(number) {
+            return number === random;
           });
 
-          if ($alreadyExist) {
+          if (alreadyExist) {
             i--;
 
             continue;
           }
 
-          $game.push($random);
+          currentGame.push(random);
         }
 
         app.selectRandomGame();
       },
 
       selectRandomGame: function selectRandomGame() {
-        var $gameButtonsDiv = doc.querySelector('[data-js="game-buttons"]');
-        var $gameButtons = $gameButtonsDiv.childNodes;
+        let gameButtonsDiv = doc.querySelector('[data-js="game-buttons"]');
+        let gameButtons = gameButtonsDiv.childNodes;
         
-        Array.prototype.map.call($gameButtons, function(button) {
-          var $buttonNumber = Number(button.textContent);
+        Array.prototype.map.call(gameButtons, function(button) {
+          let buttonNumber = Number(button.textContent);
 
-          var $haveThisNumber = $game.some(function(number) {
-            return number === $buttonNumber;
+          let haveThisNumber = currentGame.some(function(number) {
+            return number === buttonNumber;
           });
 
-          if ($haveThisNumber) {
+          if (haveThisNumber) {
             button.setAttribute('class', 'selected-game');
           }
         });
       },
 
       clearGame: function clearGame() {
-        var $gameButtonsDiv = doc.querySelector('[data-js="game-buttons"]');
-        var $gameButtons = $gameButtonsDiv.childNodes;
-        
-        $game = [];
+        if (currentGameName === '') {
+          return;
+        }
 
-        Array.prototype.map.call($gameButtons, function(button) {
+        let gameButtonsDiv = doc.querySelector('[data-js="game-buttons"]');
+        let gameButtons = gameButtonsDiv.childNodes;
+        
+        currentGame = [];
+
+        Array.prototype.map.call(gameButtons, function(button) {
           button.removeAttribute('class');
         });
       }, 
 
       showInCart: function showInCart(element) {
-        var $cartItem = doc.querySelector('[data-js="cart-item"]');
-        $cartItem.appendChild(element);
+        let cartTotalPrice = doc.querySelector('[data-js="total-price"]');
 
-        var $cartTotalPrice = doc.querySelector('[data-js="total-price"]');
-
-        var $gameNumbers = element.querySelector('[data-js="game-numbers"]');
-        var $name = element.querySelector('[data-js="game-name"]');
-        var $gamePrice = element.querySelector('[data-js="game-price"]');
-        var $divisor = element.querySelector('[data-js="divisor"]');
+        let allGameNumbers = doc.querySelectorAll('[data-js="game-numbers"]');
 
         app.gameSort();
 
-        $gameNumbers.textContent = $game.join(', ');
-        $name.textContent = $gameName;
+        let gameAlreadyExistInCart = Array.prototype.some.call(allGameNumbers, function(gameNumber) {
+          return gameNumber.textContent === currentGame.join(', ').toString();
+        });
 
-        Array.prototype.map.call(data.types, function(game) {
-          if ($gameName === game.type) {
-            $name.style.color = game.color;
-            $divisor.style.backgroundColor = game.color;
+        if (gameAlreadyExistInCart || currentGame.length === 0) {
+          return;
+        }
 
-            $gamePrice.textContent = app.convertPrice(game.price);
-            $totalPrice += game.price;
+        let cartItem = doc.querySelector('[data-js="cart-item"]');
+        cartItem.appendChild(element);
+
+        let gameNumbers = element.querySelector('[data-js="game-numbers"]');
+        let name = element.querySelector('[data-js="game-name"]');
+        let gamePrice = element.querySelector('[data-js="game-price"]');
+        let divisor = element.querySelector('[data-js="divisor"]');
+
+        gameNumbers.textContent = currentGame.join(', ');
+        name.textContent = currentGameName;
+
+        Array.prototype.map.call(apiGamesData.types, function(game) {
+          if (currentGameName === game.type) {
+            name.style.color = game.color;
+            divisor.style.backgroundColor = game.color;
+
+            gamePrice.textContent = app.convertPrice(game.price);
+            currentTotalPrice += game.price;
           }
         });
 
-        $cartTotalPrice.textContent = app.convertPrice($totalPrice);
+        cartTotalPrice.textContent = app.convertPrice(currentTotalPrice);
+
+        app.clearGame();
       },
 
       listenDelete: function listenDelete(button) {
@@ -272,18 +321,30 @@
       },
 
       deleteItem: function deleteItem(button) {
-        var $cart = button.parentElement.parentElement;
-        var $item = button.parentElement;
+        let cartTotalPrice = doc.querySelector('[data-js="total-price"]');
 
-        $cart.removeChild($item);
+        let cart = button.parentElement.parentElement;
+        let item = button.parentElement;
+        
+        let itemPrice = item.querySelector('[data-js="game-price"]');
+        let itemPriceTextContent = itemPrice.textContent;
+        let itemPriceString = itemPriceTextContent.slice(2).trim();
+        let itemPriceStringFormatted = itemPriceString.replace(/[^\d,]+/g, '').replace(',', '.');
+        let itemPriceNumber = Number(itemPriceStringFormatted);
+        
+        currentTotalPrice -= itemPriceNumber;
+
+        cartTotalPrice.textContent = app.convertPrice(currentTotalPrice);
+
+        cart.removeChild(item);
       },
 
       showGameInfos: function showGameInfos(game) {
-        var $name = doc.querySelector('[data-js="game-name"]');
-        var $gameDescription = doc.querySelector('[data-js="game-description"]');
+        let name = doc.querySelector('[data-js="game-name"]');
+        let gameDescription = doc.querySelector('[data-js="game-description"]');
 
-        $name.textContent = game.type.toUpperCase();
-        $gameDescription.textContent = game.description;
+        name.textContent = game.type.toUpperCase();
+        gameDescription.textContent = game.description;
       },
 
       showAppData: function showAppData() {
@@ -291,13 +352,15 @@
           return;
         }
 
-        data = JSON.parse(this.responseText);
+        apiGamesData = JSON.parse(this.responseText);
+
+        app.createGameButtons();
       },
 
       startCart: function startCart() {
-        var $cartTotalPrice = doc.querySelector('[data-js="total-price"]');
+        let cartTotalPrice = doc.querySelector('[data-js="total-price"]');
 
-        $cartTotalPrice.textContent = app.convertPrice($totalPrice);
+        cartTotalPrice.textContent = app.convertPrice(currentTotalPrice);
       },
 
       isReady: function isReady() {
@@ -305,13 +368,13 @@
       },
 
       gameSort: function gameSort() {
-        return $game.sort(function(n1, n2) {
+        return currentGame.sort(function(n1, n2) {
           return n1 - n2;
         });
       },
 
       convertPrice: function convertPrice(price) {
-        var formatter = new Intl.NumberFormat('pt-BR', {
+        let formatter = new Intl.NumberFormat('pt-BR', {
           style: 'currency',
           currency: 'BRL',
         });
